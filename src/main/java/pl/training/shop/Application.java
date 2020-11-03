88964a40928b2917a0b8f6b797694101e14a5c48
pziubinski <p.ziubinski@gmail.com>
2020-11-03 11:11:09 +0100

    M1L1.4 kontener spring

--- src/main/java/pl/training/shop/Application.java
+++ src/main/java/pl/training/shop/Application.java
@@ -1,20 +1,23 @@
 package pl.training.shop;
 
 import lombok.extern.java.Log;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import pl.training.shop.payments.*;
 
 @Log
 public class Application {
 
-    public static void main(String[] args) {
-        var paymentIdGenerator = new IncrementalPaymentIdGenerator();
-        var fakePaymentService = new FakePaymentService(paymentIdGenerator);
-        var paymentService = new LoggingPaymentService(fakePaymentService);
-        var paymentRequest = PaymentRequest.builder()
-                .money(LocalMoney.of(1_000))
-                .build();
-        var payment = paymentService.process(paymentRequest);
-        log.info(payment.toString());
-    }
+    public static final String BASE_PACKAGE = "pl.training.shop";
 
+    public static void main(String[] args) {
+        try (AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(BASE_PACKAGE)) {
+
+            var paymentService = applicationContext.getBean(LoggingPaymentService.class);
+            var paymentRequest = PaymentRequest.builder()
+                    .money(LocalMoney.of(1_000))
+                    .build();
+            var payment = paymentService.process(paymentRequest);
+            log.info(payment.toString());
+        }
+    }
 }
