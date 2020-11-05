33e037ceb9ce24f210fc8a7f7538583edb4e2094
pziubinski <p.ziubinski@gmail.com>
2020-11-05 16:03:50 +0100

    M3L3.1 podstawy AOP

--- src/main/java/pl/training/shop/payments/FakePaymentService.java
+++ src/main/java/pl/training/shop/payments/FakePaymentService.java
@@ -24,6 +24,7 @@ public class FakePaymentService implements PaymentService {
                 .status(PaymentStatus.STARTED)
                 .build();
         eventPublisher.publishEvent(new PaymentStatusChangedEvent(this, payment));
+        //throw new RuntimeException(); //aspekty z wyjatkiem
         return paymentRepository.save(payment);
     }
 
--- src/main/java/pl/training/shop/payments/PaymentConsoleLogger.java
+++ src/main/java/pl/training/shop/payments/PaymentConsoleLogger.java
@@ -2,8 +2,7 @@ package pl.training.shop.payments;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.*;
 import org.springframework.context.MessageSource;
 
 import java.util.Locale;
@@ -16,6 +15,21 @@ public class PaymentConsoleLogger {
     private static final String MESSAGE_KEY = "paymentInfo";
 
     private final MessageSource messageSource;
+
+    @Before(value = "@annotation(LogPayments) && args(paymentRequest)")
+    public void beforePayment(PaymentRequest paymentRequest) {
+        log.info("New payment request: " + paymentRequest);
+    }
+
+    @After("@annotation(LogPayments)")
+    public void afterPayment() {
+        log.info("After payment");
+    }
+
+    @AfterThrowing(value = "@annotation(LogPayments)", throwing = "exception")
+    public void onException(Exception exception) {
+        log.info("Payment exception: " + exception.getClass().getSimpleName());
+    }
 
     @AfterReturning(value = "@annotation(LogPayments)", returning = "payment")
     public void log(Payment payment) {
