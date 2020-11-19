a845822088ea9eabde3bc025f5f9432082ecd79d
ziubi <p.ziubinski@gmail.com>
2020-11-19 18:26:29 +0100

    M3Z3 aspekt Retry

--- src/main/java/pl/training/shop/products/ProductService.java
+++ src/main/java/pl/training/shop/products/ProductService.java
@@ -2,14 +2,17 @@ package pl.training.shop.products;
 
 import lombok.RequiredArgsConstructor;
 import pl.training.shop.common.PagedResult;
+import pl.training.shop.common.retry.Retry;
 
 @RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
 
+    @Retry
     public Product add(Product product) {
-        return productRepository.save(product);
+        throw new RuntimeException();
+        //return productRepository.save(product);
     }
 
     public PagedResult<Product> getAll(int pageNumber, int pageSize) {
